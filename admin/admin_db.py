2d205c704e8af9dafc8ccc890b5c7fadde052173
Chun-Sheng-Wang <125810287+Chun-Sheng-Wang@users.noreply.github.com>
2024-07-24 15:03:19 +0000

    1103

--- admin/admin_db.py
+++ admin/admin_db.py
@@ -56,7 +56,7 @@ def main():
         
     # 顯示更新訊息
     if st.session_state.data_updated:
-        st.success("資料已更新12")
+        st.success("資料已更新13")
         st.session_state.data_updated = False
 
 
