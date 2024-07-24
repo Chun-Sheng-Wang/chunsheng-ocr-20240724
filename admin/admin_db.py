373cdbe867d101f7291d076d48c60825ce51fbb7
Chun-Sheng-Wang <125810287+Chun-Sheng-Wang@users.noreply.github.com>
2024-07-24 13:35:37 +0000

    0935

--- admin/admin_db.py
+++ admin/admin_db.py
@@ -14,29 +14,6 @@ def fetch_data():
     conn.close()
     return df
 
-# 新增記錄
-def insert_record(id, name, password):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO USERS (ID, NAME, PASSWORD) VALUES (?, ?, ?)", (id, name, password))
-    conn.commit()
-    conn.close()
-
-# 修改記錄
-def update_record(id, name, password):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("UPDATE USERS SET NAME=?, PASSWORD=? WHERE ID=?", (name, password, id))
-    conn.commit()
-    conn.close()
-
-# 刪除記錄
-def delete_record(id):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM USERS WHERE ID=?", (id,))
-    conn.commit()
-    conn.close()
 
 def main():
     #st.markdown("### 資料表處理")    
@@ -47,8 +24,7 @@ def main():
     with st.form("my_form"):    
         data = fetch_data()
         edited_data = st.data_editor(data, num_rows="dynamic", use_container_width=True)
-        #submitted = st.form_submit_button("保存修改") 
-        submitted = st.button("保存修改")  
+        submitted = st.form_submit_button("保存修改")  
 
 
     # 保存編輯後的數據
